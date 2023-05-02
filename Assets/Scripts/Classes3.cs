ab9a455b8806656e13b0158f8c8d39b7884e5b9e
miladinovangel <miladinovangel95@gmail.com>
2023-05-02 21:18:58 +0200

    examples with references + unity 3d basics

--- Assets/Scripts/Classes3.cs
+++ Assets/Scripts/Classes3.cs
@@ -6,14 +6,56 @@ public class Classes3 : MonoBehaviour
 {
     private void Start()
     {
-        Number object1 = new Number();
-        object1 = null;
+        //Number object1 = new Number();
+        //object1.num = 3;
+        ////object1 = null;
 
-        Number object2 = object1; // 2te promenlivi imaat ista referenca
-        if (object2 != null)
-            object2.num = 5;// null reference exception
+        //Number object2 = object1; // 2te promenlivi imaat ista referenca
+        //object2.num = 103;
+        ////if (object2 != null)
+        ////    object2.num = 5;// null reference exception
 
-        Debug.Log(object1.num);
+        //Debug.Log(object1.num);
+        //string string1 = "Hello World";
+        //string string2 = string1;
+        ////-------------------------------
+        ////string1 = "H";
+
+        //string2 = "";
+        //if (string2 != null) // dokolku ima referenca
+        //{
+        //    Debug.Log(string2);
+        //}
+        //Debug.Log(string1);
+        //Debug.Log(string2);
+        // string1 ?
+        // string2 ?
+        //Number object1 = new Number();
+        //object1.num = 10;
+        //Test1(object1);// prakanje na objekt po referenca
+        //Debug.Log(object1.num);
+
+        //int number = 3;
+        //Test2(number);
+        //Debug.Log(number);
+
+        //string someString = "Hello world";
+        //Test3(someString);
+        //Debug.Log(someString);
+    }
+
+    private void Test1(Number number) // vrednosta se praka preku referenca
+    {
+        number.num = 11;
+    }
+    private void Test2(int number)// vrednosta e od tip int, i vrednosta si se avtomatski duplicira
+    {
+        number = 0;
+    }
+
+    private void Test3(string s)
+    {
+        s = "";
     }
 }
 
