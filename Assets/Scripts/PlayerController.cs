a9332d7b47dd53ae4235fde5735c9772a221440a
miladinovangel <miladinovangel95@gmail.com>
2023-05-16 21:27:06 +0200

    refactored last logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
-    [SerializeField]
-    private GameObject bulletPrefab;
-    [SerializeField]
-    private Transform bulletSpawnPosition;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPosition;
 
     void Update()
     {
-        // kako da implementirame dvizenje
-        //Input.GetAxis("Horizontal");
-        //Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -32,30 +24,25 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //bulletPrefab.transform.position = transform.position;
-            GameObject bulletInstance = Instantiate(bulletPrefab); // kreirame instanca od objektot bullet prefab
+            GameObject bulletInstance = Instantiate(bulletPrefab);
             bulletInstance.transform.position = bulletSpawnPosition.position;
         }
-
-
     }
-    void ResetSpeed ()
-    {
-        speed /= 1.2f;
-    }
+    
     private void OnCollisionEnter(Collision other)
     {
-        
-        if (other.gameObject.tag == "RotatingBox")
+        if (other.gameObject.tag == "SpeedPowerupCollectable")
         {
             Destroy(other.gameObject);
             speed *= 1.2f;
             Invoke("ResetSpeed", 3f);
         }
-        
-      
+    }
+
+    private void ResetSpeed()
+    {
+        speed /= 1.2f;
     }
 }
